bf18f34e1c099496bf559ac77dc46fecdd736c19
TANNHAN\tan_nhan <trantanthanhnhan@gmail.com>
2024-03-12 23:25:04 +0700

    create another table

--- database.sql
+++ database.sql
@@ -14,6 +14,13 @@ CREATE TABLE users(
     facebook_account_id INT DEFAULT 0,
     google_account_id INT DEFAULT 0
 );
+ALTER TABLE users ADD COLUMN role_id INT;
+
+CREATE TABLE roles(
+    id INT PRIMARY KEY,
+    name varchar(20) not null,
+)
+ALTER TABLE users ADD FOREIGN KEY(role_id) REFERENCES roles(id);
 
 CREATE TABLE tokens(
     id int PRIMARY KEY AUTO_INCREMENT,
@@ -25,7 +32,7 @@ CREATE TABLE tokens(
     user_id int,
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
-
+--login tu facebook or google
 CREATE TABLE social_accounts(
     id int PRIMARY key AUTO_INCREMENT,
     provider VARCHAR(20) not null comment 'Ten nha social network',
@@ -33,5 +40,58 @@ CREATE TABLE social_accounts(
     email varchar(150) not null comment 'email tai khoan',
     name varchar(100) not null comment 'ten nguoi dung',
     user_id int,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+--bang danh muc san pham
+CREATE TABLE categories(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL DEFAULT '' comment 'ten danh muc, do dien tu'
+);
+
+--bang chua san pham(Product)
+CREATE TABLE products(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name varchar(350) comment 'ten san pham',
+    price FLOAT NOT NULL CHECK(price >= 0),
+    thumbnail varchar(300) DEFAULT '',
+    description LONGTEXT DEFAULT '',
+    created_at DATETIME,
+    updated_at DATETIME,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+--dat hang
+CREATE TABLE orders(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id int,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    fullname varchar(100) DEFAULT '',
+    email varchar(100) DEFAULT '',
+    phone_number varchar(20) NOT NULL,
+    address varchar(100) not null,
+    note varchar(100) DEFAULT '',
+    order_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status varchar(20),
+    total_money FLOAT CHECK(total_money >=0)
+);
+ALTER TABLE orders ADD COLUMN shipping_method VARCHAR(100);
+ALTER TABLE orders ADD COLUMN shipping_adrress VARCHAR(200);
+ALTER TABLE orders ADD COLUMN shipping_date DATE;
+ALTER TABLE orders ADD COLUMN tracking_number VARCHAR(100);
+ALTER TABLE orders ADD COLUMN payment_method VARCHAR(100);
+ALTER TABLE orders ADD COLUMN active TINYINT(1);
+
+ALTER TABLE orders MODIFY COLUMN status ENUM('pending','processing','shipped','delivered', 'cancelled') comment 'Trang thai don hang';
+
+CREATE TABLE order_details(
+    id INT PRIMARY KEY,
+    order_id INT,
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    product_id INT,
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    price FLOAT CHECK (price >= 0),
+    number_of_product INT CHECK (number_of_product >= 0),
+    total_money FLOAT CHECK (number_of_product >= 0),
+    color VARCHAR(20) DEFAULT ''
 );
